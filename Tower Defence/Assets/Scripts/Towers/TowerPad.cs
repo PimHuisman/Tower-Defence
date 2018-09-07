cec39232f5f05b79d75e659d82d77867f2954811
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-09-07 15:33:00 +0200

    Updated my tower pad logic

--- Tower Defence/Assets/Scripts/Towers/TowerPad.cs
+++ Tower Defence/Assets/Scripts/Towers/TowerPad.cs
@@ -1,16 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerPad : MonoBehaviour {
+	public MouseToWorldSpace myMouse;
+	private RaycastHit mouseHit;
+
+	public GameObject buyPanel;
+	public GameObject uiPoint;
+
 
 	// Use this for initialization
 	void Start () {
-		
+		buyPanel.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		CheckMouse();
 		
+	}
+
+	void CheckMouse () {
+		mouseHit = myMouse.mouseHit;
+
+		if(mouseHit.collider == gameObject.GetComponent<Collider>()) {
+			if(Input.GetButtonDown("Fire1")) {
+				ShowPanel();
+			}
+		}
+
+		if(Input.GetButtonDown("Fire2")) {
+		HidePanel();
+		}
+	}
+
+	void ShowPanel () {
+		buyPanel.SetActive(true);
+		
+		Vector3 panelPos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		buyPanel.transform.position = panelPos;
+	}
+
+	void HidePanel () {
+		buyPanel.SetActive(false);
 	}
 }
---
+++ Tower Defence/Assets/Scripts/UI/MouseToWorldSpace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseToWorldSpace : MonoBehaviour {
+	private Ray mouseRay;
+	public RaycastHit mouseHit;
+
+	// Update is called once per frame
+	void Update () {
+		mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if(Physics.Raycast(mouseRay, out mouseHit)) {
+			
+		}
+
+	}
+}
