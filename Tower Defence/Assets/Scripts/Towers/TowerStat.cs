0001bb882a3f9bcb590af56d9895803f7c456b2c
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-09-07 16:17:05 +0200

    tower
    tower

---
+++ Tower Defence/Assets/Scripts/Towers/TowerStat.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tower", menuName = "Tower")]
+public class TowerStat : ScriptableObject
+{
+    public int cost;
+    public float range;
+    public int damage;
+    public float fireRate;
+
+}
