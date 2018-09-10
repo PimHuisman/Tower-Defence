3ce4a352652a74c471baba0eb2041676a8c6b389
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-09-10 10:31:24 +0200

    add tower
    tower

---
+++ Tower Defence/Assets/Scripts/Towers/TowerBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBehaviour : MonoBehaviour 
+{
+	List<Transform> targets = new List<Transform>();
+	 
+	void Start () 
+	{
+
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.transform.tag == "Enemy")
+		{
+			targets.Add(other.transform);
+			print("Enemy is detected");
+		}
+	}
+
+	void OnTriggerExit(Collider other) 
+	{
+		if (other.transform.tag == "Enemy")
+		{
+			targets.Remove(other.transform);
+			print("Enemy got away");
+		}
+	}
+}
