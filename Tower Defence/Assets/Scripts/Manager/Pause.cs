83df0513181eb8785bf0641de4fdd435188163a9
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-09-16 22:05:48 +0200

    pause menu(Script)
    pause menu (Script)

---
+++ Tower Defence/Assets/Scripts/Manager/Pause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour 
+{
+	[SerializeField] GameObject pauseMenu;
+	bool ispaused;
+	void Start () 
+	{
+		ispaused = false;
+	}
+	
+	void Update () 
+	{
+		if (Input.GetButtonDown("Cancel"))
+		{
+			ispaused = !ispaused;
+		}
+
+		if (ispaused)
+		{
+			Time.timeScale = 0;
+			pauseMenu.SetActive(true);
+		}
+		else
+		{
+			Time.timeScale = 1;
+			pauseMenu.SetActive(false);
+		}
+	}
+}
