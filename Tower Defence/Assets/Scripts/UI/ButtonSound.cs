daaa7e5c0b92ac781922b0e4dadd138b25783308
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-09-17 11:19:37 +0200

    Added button feedback

---
+++ Tower Defence/Assets/Scripts/UI/ButtonSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioClip mainButtonSound;
+	
+    public AudioSource source;
+
+    public void PlaySound()
+    {
+        source.clip = mainButtonSound;
+        source.Play();
+    }
+}
