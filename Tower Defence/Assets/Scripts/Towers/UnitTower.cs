1b9e9fb8192b6ce762d5945bf6504e9f1e4fd64c
PimHuisman <33859395+PimHuisman@users.noreply.github.com>
2018-10-02 07:40:22 +0200

    bigger path

---
+++ Tower Defence/Assets/Scripts/Towers/UnitTower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitTower : TowerBehaviour
+{
+	[SerializeField] GameObject unit;
+	[SerializeField] int unitMaxAmount;
+	[SerializeField] Transform areaPos;
+	public List<Transform> unitList = new List<Transform>();
+	public override void Start() 
+    {
+		GenerateArrows();
+    }
+
+    public override void Update()
+    {
+
+    }
+
+    public override void Shoot()
+    {
+
+    }
+    public override void GenerateArrows()
+    {
+		for (int i = 0; i < unitMaxAmount; i++)
+		{
+			Instantiate(unit, areaPos.position, areaPos.rotation);
+		}
+    }
+}
