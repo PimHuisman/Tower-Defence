3add8e8db520a16338a703a213e5b9faa1e6ffc5
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-10-12 15:42:24 +0200

    Refined camera movement

--- Tower Defence/Assets/Scripts/Camera/CameraMovement.cs
+++ Tower Defence/Assets/Scripts/Camera/CameraMovement.cs
@@ -7,10 +7,13 @@ public class CameraMovement : MonoBehaviour {
 	public float maxSize;
 	public float startSize;
 	public float zoomSensetivity;
-	public float horMoveSensetivity;
-	public float verMoveSensetivity;
+	public float moveSensetivity;
 	public GameObject camObject;
 	private Camera camCamera;
+
+	public GameObject ground;
+	private Bounds groundBounds;
+	private Vector3 extends;
 
 	void Start () {
 		//Get Camera.
@@ -18,9 +21,72 @@ public class CameraMovement : MonoBehaviour {
 
 		//Set size to start size
 		SetSize (startSize);
+
+		GetBounds ();
 	}
 
 	void Update () {
+		GetSizeChange ();
+		GetPositionChange ();
+	}
+
+	void GetBounds () {
+		Renderer groundRend = ground.GetComponent<Renderer> ();
+		Bounds groundBounds = groundRend.bounds;
+		print (groundBounds);
+
+		extends = groundBounds.extents;
+
+	}
+
+	void GetPositionChange () {
+		Vector3 oldPos = transform.position;
+
+		//Get input
+		float verticalInput = Input.GetAxisRaw ("Vertical");
+		float horizontalInput = Input.GetAxisRaw ("Horizontal");
+
+		transform.Translate (Vector3.forward * verticalInput * moveSensetivity);
+		transform.Translate (Vector3.right * horizontalInput * moveSensetivity);
+		Vector3 newPos = transform.position;
+
+		//Only check if object is out of bounds if the pos has changed
+		if (oldPos != newPos) {
+			CheckExtends ();
+
+		}
+	}
+
+	void CheckExtends () {
+		//X Axis
+		if (extends.x >= transform.position.x) {
+			print ("Transfrom is in positive extends.");
+		} else {
+			transform.position = new Vector3 (extends.x, transform.position.y, transform.position.z);
+		}
+
+		if (-extends.x <= transform.position.x) {
+			print ("Transform is in negative extends.");
+		} else {
+			transform.position = new Vector3 (-extends.x, transform.position.y, transform.position.z);
+		}
+
+		//Z Axis
+		if (extends.z >= transform.position.z) {
+			print ("Transfrom is in positive extends.");
+		} else {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, extends.z);
+		}
+
+		if (-extends.z <= transform.position.z) {
+			print ("Transform is in negative extends.");
+		} else {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, -extends.z);
+		}
+
+	}
+
+	void GetSizeChange () {
 		//Get scroll wheel input
 		float scrollAxis = Input.GetAxis ("Mouse ScrollWheel");
 
@@ -28,25 +94,10 @@ public class CameraMovement : MonoBehaviour {
 			//Set size of camera to new size
 			SetSize (camCamera.orthographicSize + -scrollAxis * zoomSensetivity);
 		}
-
-		//Get WASD keys
-		float verticalAxis = Input.GetAxisRaw ("Vertical");
-		float horizontalAxis = Input.GetAxisRaw ("Horizontal");
-		
-		if (horizontalAxis != 0) {
-			transform.Translate (-transform.right * horizontalAxis * horMoveSensetivity * Time.deltaTime);
-		}
-		if(verticalAxis != 0) {
-			Quaternion oldRotation = transform.rotation;	
-			
-			transform.Translate(-new Vector3(transform.rotation.x, transform.rotation.y, 0.0f) * verticalAxis * verMoveSensetivity * Time.deltaTime);
-			//transform.rotation = oldRotation;
-			//transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-		}
-
 	}
 
 	void SetSize (float size) {
+
 		if (size > maxSize) {
 			size = maxSize;
 		} else if (size < minSize) {
