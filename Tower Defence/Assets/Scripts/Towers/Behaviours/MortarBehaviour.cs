92533e2bc1a13f4fce102470a3b58b0d019b5e61
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-10-29 10:28:43 +0100

    Added mortar behaviour

---
+++ Tower Defence/Assets/Scripts/Towers/Behaviours/MortarBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarBehaviour : TowerBehaviour {
+
+    public  void Start()
+    {
+        SetStats();
+		StartCoroutine(ShootRoutine());
+    }
+
+    public  void Update()
+    {
+        CheckEnemies();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        base.Enter(other);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        base.Exit(other);
+    }
+
+    public override void CheckEnemies()
+    {
+        base.CheckEnemies();
+    }
+}
