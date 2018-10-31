db0605f81bc6b4a25bcc36874270a2c7b745bdbf
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-10-31 11:48:26 +0100

    Pushed main menu main

---
+++ Tower Defence/Assets/Scripts/UI/MouseOverBanner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverBanner : MonoBehaviour {
+	Animator myAnimator;
+	float animSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		myAnimator = gameObject.GetComponent<Animator> ();
+		animSpeed = myAnimator.speed;
+		myAnimator.SetFloat ("Speed", 0);
+	}
+
+	void Update () {
+
+	}
+
+	void OnMouseEnter () {
+		SetAnimSpeed (1);
+	}
+
+	public void OnMouseExit () {
+		SetAnimSpeed (-1);
+	}
+
+	public void SetAnimSpeed (float speed) {
+		print ("Setting float speed to " + speed);
+		myAnimator.SetFloat ("Speed", speed);
+	}
+}
