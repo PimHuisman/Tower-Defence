9d7a5e6af290de0c1e3f81c9c0e0d0f79571e534
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-11-06 12:42:55 +0100

    Dab dab dab, dab dab? Dab!

--- Tower Defence/Assets/Scripts/Towers/TempleStats.cs
+++ Tower Defence/Assets/Scripts/Towers/TempleStats.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TempleStats : MonoBehaviour
-{
+public class TempleStats : MonoBehaviour {
     public int beginHealth;
     float currentHealth;
     public float damage;
@@ -12,49 +11,52 @@ public class TempleStats : MonoBehaviour
     public Image currentHealthImg;
 
     public bool doDamage;
+    public bool hasBeenFound;
 
-    void Start()
-    {
-        endScreen = GameObject.FindGameObjectWithTag("Manager").GetComponent<EndScreen>();
+    void Start () {
+        //endScreen = GameObject.FindGameObjectWithTag("Manager").GetComponent<EndScreen>(); 
+        if (GameObject.FindGameObjectWithTag ("Manager") != null) {
+            hasBeenFound = true;
+        } else {
+            hasBeenFound = false;
+        }
         currentHealth = beginHealth;
-        print("Current health start " + currentHealth);
+        print ("Current health start " + currentHealth);
     }
 
-    void Update()
-    {
+    void Update () {
+        if (hasBeenFound == false) {
+            if (GameObject.FindGameObjectWithTag ("Manager") != null) {
+                print("Found the object! DAB!");
+                hasBeenFound = true;
+            }
+        }
 
     }
 
-    public void DamageMe(float dmg)
-    {
+    public void DamageMe (float dmg) {
         currentHealth -= dmg;
 
-        CheckHealth();
-        ChangeUI();
+        CheckHealth ();
+        ChangeUI ();
     }
 
-    public void CheckHealth()
-    {
-        if (currentHealth <= 0)
-        {
-            endScreen.Die();
+    public void CheckHealth () {
+        if (currentHealth <= 0) {
+            endScreen.Die ();
         }
     }
 
-    public void ChangeUI()
-    {
+    public void ChangeUI () {
         float percentage;
 
         percentage = currentHealth / beginHealth;
 
-        print("Perc " + percentage);
+        print ("Perc " + percentage);
 
-        if (percentage <= 0)
-        {
+        if (percentage <= 0) {
             currentHealthImg.fillAmount = 0;
-        }
-        else
-        {
+        } else {
             currentHealthImg.fillAmount = percentage;
         }
 
