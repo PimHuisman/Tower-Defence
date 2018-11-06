6c1302b6be00c5f7cd83f842a33e5a7674630e57
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-11-06 17:02:35 +0100

    Ik ga pushen pushen pushen psuh

--- Tower Defence/Assets/Scripts/Towers/TempleStats.cs
+++ Tower Defence/Assets/Scripts/Towers/TempleStats.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TempleStats : MonoBehaviour
-{
+public class TempleStats : MonoBehaviour {
     public int beginHealth;
     float currentHealth;
+
+    public Text healthText;
+    public Text healthTextShadow;
     public float damage;
     public EndScreen endScreen;
     public Image currentHealthImg;
@@ -14,27 +16,22 @@ public class TempleStats : MonoBehaviour
     public bool doDamage;
     public bool hasBeenFound;
 
-    void Start()
-    {
+    void Start () {
         //endScreen = GameObject.FindGameObjectWithTag("Manager").GetComponent<EndScreen>(); 
-        if (GameObject.FindGameObjectWithTag("Manager") != null)
-        {
+        if (GameObject.FindGameObjectWithTag ("Manager") != null) {
             hasBeenFound = true;
-        }
-        else
-        {
+        } else {
             hasBeenFound = false;
         }
         currentHealth = beginHealth;
-        print("Current health start " + currentHealth);
+        print ("Current health start " + currentHealth);
+
+        ChangeUI();
     }
 
-    void Update()
-    {
-        if (hasBeenFound == false)
-        {
-            if (GameObject.FindGameObjectWithTag("Manager") != null)
-            {
+    void Update () {
+        if (hasBeenFound == false) {    
+            if (GameObject.FindGameObjectWithTag ("Manager") != null) {
                 print("Found the object! DAB!");
                 hasBeenFound = true;
             }
@@ -42,38 +39,25 @@ public class TempleStats : MonoBehaviour
 
     }
 
-    public void DamageMe(float dmg)
-    {
+    public void DamageMe (float dmg) {
         currentHealth -= dmg;
 
-        CheckHealth();
-        ChangeUI();
+        CheckHealth ();
+        ChangeUI ();
     }
 
-    public void CheckHealth()
-    {
-        if (currentHealth <= 0)
-        {
-            endScreen.Die();
+    public void CheckHealth () {
+        if (currentHealth <= 0) {
+            endScreen.Die ();
         }
     }
 
-    public void ChangeUI()
-    {
-        float percentage;
+    public void ChangeUI () {
+        string s = beginHealth.ToString() + " / " + currentHealth.ToString();
+        print("String s is " + s);
+        healthText.text = s;
+        healthTextShadow.text = s;
 
-        percentage = currentHealth / beginHealth;
-
-        print("Perc " + percentage);
-
-        if (percentage <= 0)
-        {
-            currentHealthImg.fillAmount = 0;
-        }
-        else
-        {
-            currentHealthImg.fillAmount = percentage;
-        }
 
     }
 }
