e3b641b4c39865572816f499f0e5e90408017a14
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2018-11-08 09:14:40 +0100

    Made the settings working

--- Tower Defence/Assets/Scripts/UI/SettingsMenu.cs
+++ Tower Defence/Assets/Scripts/UI/SettingsMenu.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class SettingsMenu : MonoBehaviour
-{
-	public AudioMixer mainMixer;
-    public void SetVolume(float volume)
-    {
-		mainMixer.SetFloat("MasterMixer", volume);
-    }
+public class SettingsMenu : MonoBehaviour {
+  public AudioMixer mainMixer;
+
+  public void SetVolume (float volume) {
+    mainMixer.SetFloat ("MasterMixer", volume);
+  }
+
+  public void SetRes1920 () {
+    Screen.SetResolution (1920, 1080, false);
+  }
+
+  public void SetRes1280 () {
+    Screen.SetResolution (1920, 1080, false);
+  }
+
 }
